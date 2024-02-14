af4363eade4de5345ac50031aa800ffe2cee305b
ausov-hacken <ausov@hacken.io>
2024-02-14 21:33:08 +0200

    fix: using ABIRecord instead of Dict

--- src/sentinel/db/contract/abi/standard.py
+++ src/sentinel/db/contract/abi/standard.py
@@ -1,4 +1,3 @@
-
 import json
 import logging
 
@@ -9,7 +8,7 @@ from .erc20 import ERC20
 from .erc721 import ERC721
 from .erc1155 import ERC1155
 
-from sentinel.models.contract import ABISignature
+from sentinel.models.contract import ABIRecord, ABISignature
 from sentinel.db.contract.utils import to_signature_record
 
 
@@ -17,64 +16,67 @@ logger = logging.getLogger(__name__)
 
 
 class StandardABISignatures:
-    '''
+    """
     Standard ABI Signatures
 
     - ERC-20
     - ERC-721
     - ERC-1155
-    '''
+    """
+
     def __init__(self, standards: List[str] = list()) -> None:
-        '''
+        """
         Standard ABI Signatures Init
-        '''
+        """
         self._db = []
         self._standards = standards
         self.update(standards=self._standards)
 
     @property
     def total_records(self):
-        '''
+        """
         returns total records number in database
-        '''
+        """
         return len(self._db)
 
     def update(self, standards: List[str] = list()) -> None:
-        '''
+        """
         Update ABI Signatures
-        '''
+        """
         if len(standards) == 0:
             return
-        
+
         for standard in standards:
-            if standard == 'ERC20':
-                self._load('ERC20', json.loads(ERC20))
-            elif standard == 'ERC721':
-                self._load('ERC721', json.loads(ERC721))
-            elif standard == 'ERC1155':
-                self._load('ERC1155', json.loads(ERC1155))
+            if standard == "ERC20":
+                self._load("ERC20", json.loads(ERC20))
+            elif standard == "ERC721":
+                self._load("ERC721", json.loads(ERC721))
+            elif standard == "ERC1155":
+                self._load("ERC1155", json.loads(ERC1155))
             else:
-                logger.warning(f'Unknown standard name, {standard}')
+                logger.warning(f"Unknown standard name, {standard}")
 
-    def _load(self, standard: str,  abi_records: List[Dict]) -> None:
-        '''
+    def _load(self, standard: str, abi_records: List[Dict]) -> None:
+        """
         Load ABI signatures to local database
-        '''
+        """
         for abi_record in abi_records:
-            self._db.append(to_signature_record(standard, abi_record))
+            self._db.append(to_signature_record(standard, ABIRecord(**abi_record)))
 
-
-    def search(self, standard: str = None, 
-               signature_type: str = None, 
-               signature_hash: str = None) -> Iterator[ABISignature]:
-        '''
+    def search(
+        self,
+        standard: str = None,
+        signature_type: str = None,
+        signature_hash: str = None,
+    ) -> Iterator[ABISignature]:
+        """
         Search signatures
-        '''
+        """
         for signature in self._db:
             if standard is not None:
                 if signature.contract_address != standard:
                     continue
             if signature_type is not None:
                 if signature.type != signature_type:
-                    continue 
+                    continue
             yield signature
