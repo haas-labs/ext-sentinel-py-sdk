1e0b5b3372d8cafa4b96336bb749ff41e5eef559
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-02-28 16:08:58 +0200

    feat: events http rest channel (#85)
    * feat: events http channel
    * fix: token passing to header

---
+++ src/sentinel/channels/http/outbound.py
@@ -0,0 +1,35 @@
+import logging
+
+from typing import Dict
+
+from sentinel.models.event import Event
+from sentinel.channels.http.common import OutboundHTTPChannel
+
+logger = logging.getLogger(__name__)
+
+
+class OutboundEventsChannel(OutboundHTTPChannel):
+    """
+    Outbound Events HTTP/REST Channel
+    """
+    def __init__(self, name: str, metadata: Dict = dict(), **kwargs) -> None:
+        """
+        Event Kafka Channel
+        """
+        super().__init__(name, record_type="sentinel.models.event.Event", **kwargs)
+        self._default_metadata = metadata.copy()
+
+    async def send(self, msg: Event) -> None:
+        """
+        Send Event via HTTP/REST interface
+        """
+        # Add default metadata fields only if there are no such fields
+        for k, v in self._default_metadata.items():
+            if k not in msg.metadata:
+                msg.metadata[k] = v
+
+        if not isinstance(msg, Event):
+            logger.error(f"Incorrect message type, expected: Event, detected: {type(msg)}")
+
+        await super().send(msg)
+
