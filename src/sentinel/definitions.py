d1c5c50a71bec5d1b590ff60557d091a95c37496
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-03-12 21:56:35 +0200

    feat: sentinel definitions (#131)
    * refact: rename const to codes
    * feat: add sentinel definitions
    * feat: add extended blockchain model
    * refact: remove comments

---
+++ src/sentinel/definitions.py
@@ -0,0 +1,15 @@
+from sentinel.models.blockchain import Blockchain
+
+# https://chainlist.org/
+
+BLOCKCHAIN = {
+    "ethereum": Blockchain(
+        network="ethereum", chain_id=1, short_name="eth", description="Ethereum Mainnet", currency="ETH"
+    ),
+    "bsc": Blockchain(
+        network="bsc", chain_id=56, short_name="bsc", description="BNB Smart Chain Mainnet", currency="BNB"
+    ),
+    "arbitrum": Blockchain(
+        network="arbitrum", chain_id=42161, short_name="arb", description="Arbitrum One", currency="ETH"
+    ),
+}
