d8213ee0dda17c8d023cbd7869edcfd6fe215c32
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-03-27 11:52:09 +0200

    feat: optimize sentry channels and databases (#182)
    * feat: optimize channels and databases activation
    * feat: remove outdated code
    * feat: use logger name and process name
    * test: update sentry tests according to latest optimization changes

--- tests/sentry/test_sentry_tx_detector.py
+++ tests/sentry/test_sentry_tx_detector.py
@@ -1,15 +1,19 @@
+from sentinel.models.project import ProjectSettings
 from sentinel.sentry.transaction import TransactionDetector
 
 
 def test_tx_detector_init():
     detector = TransactionDetector(
         name="TestTXDetector",
+        settings=ProjectSettings(),
         parameters={
             "network": "ethereum",
         },
     )
+    detector.activate()
     assert isinstance(detector, TransactionDetector), "Incorrect tx detector type"
-    assert detector.name == "eth://TestTXDetector", "Incorrect detector name"
+    assert detector.name == "TestTXDetector", "Incorrect detector name"
+    assert detector.logger_name == "eth://TestTXDetector", "Incorrect detector logger name"
     assert (
         detector.description
         == "The base detector which can be used for mointoring activities in transactions stream and publishing results to events stream"
