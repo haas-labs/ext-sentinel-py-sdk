c715e8cc4d7ff0f7d11ecf5424d21eaf89be0062
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-04-02 18:21:11 +0300

    feat: use full network name in prefix (#199)
    * feat: add base and optimism chains
    * feat: use full network name as prefix & update tests

--- src/sentinel/definitions.py
+++ src/sentinel/definitions.py
@@ -3,16 +3,22 @@ from sentinel.models.blockchain import Blockchain
 # https://chainlist.org/
 
 BLOCKCHAIN = {
-    "ethereum": Blockchain(
-        network="ethereum", chain_id=1, short_name="eth", description="Ethereum Mainnet", currency="ETH"
-    ),
-    "bsc": Blockchain(
-        network="bsc", chain_id=56, short_name="bsc", description="BNB Smart Chain Mainnet", currency="BNB"
+    "anvil": Blockchain(
+        network="anvil", chain_id=31337, short_name="anv", description="Anvil Testnet", currency="ETH"
     ),
     "arbitrum": Blockchain(
         network="arbitrum", chain_id=42161, short_name="arb", description="Arbitrum One", currency="ETH"
     ),
-    "anvil": Blockchain(
-        network="anvil", chain_id=31337, short_name="anv", description="Anvil Testnet", currency="ETH"
+    "base": Blockchain(
+        network="base", chain_id=8453, short_name="base", description="Base", currency="ETH"
+    ),
+    "bsc": Blockchain(
+        network="bsc", chain_id=56, short_name="bsc", description="BNB Smart Chain Mainnet", currency="BNB"
+    ),
+    "ethereum": Blockchain(
+        network="ethereum", chain_id=1, short_name="eth", description="Ethereum Mainnet", currency="ETH"
+    ),
+    "optimism": Blockchain(
+        network="optimism", chain_id=10, short_name="op", description="OP Mainnet", currency="ETH"
     ),
 }
--- tests/sentry/test_sentry_tx_detector.py
+++ tests/sentry/test_sentry_tx_detector.py
@@ -13,7 +13,7 @@ def test_tx_detector_init():
     detector.activate()
     assert isinstance(detector, TransactionDetector), "Incorrect tx detector type"
     assert detector.name == "TestTXDetector", "Incorrect detector name"
-    assert detector.logger_name == "eth://TestTXDetector", "Incorrect detector logger name"
+    assert detector.logger_name == "ethereum://TestTXDetector", "Incorrect detector logger name"
     assert (
         detector.description
         == "The base detector which can be used for mointoring activities in transactions stream and publishing results to events stream"
