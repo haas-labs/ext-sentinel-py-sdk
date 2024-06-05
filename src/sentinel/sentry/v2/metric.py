136ac60a0ae6aa0b0fe40bef17eb29c034285587
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-06-05 10:16:55 +0300

    feat: update metrics server to consume metrics from queue (#271)
    * refact: rename metrics queue to just queue
    * refact: rename metrics vars - queue, registry, channel
    * test: rename metrics queue to just queue
    * feat: add from_settings method for metrics server

--- src/sentinel/sentry/v2/metric.py
+++ src/sentinel/sentry/v2/metric.py
@@ -7,6 +7,7 @@ from aiohttp import web
 from sentinel.core.v2.sentry import AsyncCoreSentry
 from sentinel.metrics.core import MetricDatabase, MetricQueue
 from sentinel.metrics.formatter import PrometheusFormattter
+from sentinel.models.sentry import Sentry
 from sentinel.utils.logger import get_logger
 
 
@@ -41,6 +42,19 @@ class MetricServer(AsyncCoreSentry):
         self._port = self.parameters.get("port", 9090)
         self._db = MetricDatabase(retention_period=self._retention_period_secs)
 
+    @classmethod
+    def from_settings(cls, settings: Sentry, **kwargs):
+        queue = kwargs.pop("metrics", None)
+        return cls(
+            name=settings.name,
+            description=settings.description,
+            restart=settings.restart,
+            schedule=settings.schedule,
+            parameters=settings.parameters,
+            metrics=queue,
+            **kwargs,
+        )
+
     def current_time(self) -> int:
         """
         return current time in milliseconds
@@ -62,17 +76,17 @@ class MetricServer(AsyncCoreSentry):
         await app_runner.setup()
         return web.TCPSite(runner=app_runner, host=self._host, port=self._port)
 
-    async def _run(self) -> None:
+    async def processing(self) -> None:
         self.init()
         try:
             srv = await self.create_web_server()
             await srv.start()
-            metric_processor = asyncio.create_task(self.process(), name="MetricsProcessor")
+            metric_processor = asyncio.create_task(self.handle_incoming_metrics(), name="MetricsProcessor")
             await asyncio.gather(metric_processor)
         finally:
             self.logger.info("Metric Server completed")
 
-    async def process(self):
+    async def handle_incoming_metrics(self):
         last_cleanup_time = self.current_time()
         while True:
             metrics = await self.metrics_queue.receive()
--- tests/channels/metrics/test_inbound_metric_channel.py
+++ tests/channels/metrics/test_inbound_metric_channel.py
@@ -6,22 +6,20 @@ from sentinel.metrics.core import MetricQueue
 
 
 def test_inbound_metric_channel_init():
-    channel = InboundMetricChannel(id="metrics/consumer", name="metrics", metric_queue=MetricQueue())
+    channel = InboundMetricChannel(id="metrics/consumer", name="metrics", queue=MetricQueue())
     assert isinstance(channel, InboundMetricChannel), "Incorrect channel type"
 
 
 def test_inbound_metric_channel_from_settings():
-    channel = InboundMetricChannel.from_settings(
-        settings=Channel(id="metrics/consumer", type=""), metric_queue=MetricQueue()
-    )
+    channel = InboundMetricChannel.from_settings(settings=Channel(id="metrics/consumer", type=""), queue=MetricQueue())
     assert isinstance(channel, InboundMetricChannel), "Incorrect channel type"
 
 
 @pytest.mark.asyncio
 async def test_outbound_metric_channel_send():
     class MetricsConsumer(InboundMetricChannel):
-        def __init__(self, id: str, metric_queue: MetricQueue, name: str = None, **kwargs) -> None:
-            super().__init__(id=id, metric_queue=metric_queue, name=name, **kwargs)
+        def __init__(self, id: str, queue: MetricQueue, name: str = None, **kwargs) -> None:
+            super().__init__(id=id, queue=queue, name=name, **kwargs)
             self.metrics = list()
 
         async def on_metric(self, metric: MetricModel):
@@ -33,7 +31,7 @@ async def test_outbound_metric_channel_send():
     )
     channel = MetricsConsumer.from_settings(
         settings=Channel(id="metrics/consumer", type="", parameters={"stop_after": 1}),
-        metric_queue=queue,
+        queue=queue,
     )
     await queue.send(total_requests)
     await channel.run()
