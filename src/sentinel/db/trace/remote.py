6f836318a098a2d8a92d7d848908fef13a2b0ead
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-06-19 11:19:33 +0300

    fix: from settings method for remote trace db (#306)

--- src/sentinel/db/trace/remote.py
+++ src/sentinel/db/trace/remote.py
@@ -1,7 +1,8 @@
-from .common import CommonTraceDB
-
 from sentinel.formats.trace import Trace
+from sentinel.models.database import Database
 from sentinel.services.rpc.tracer import Tracer
+
+from .common import CommonTraceDB
 
 
 class RemoteTraceDB(CommonTraceDB):
@@ -12,6 +13,24 @@ class RemoteTraceDB(CommonTraceDB):
         Remote Trace DB Init
         """
         self.tracer = Tracer(endpoint=endpoint_url, network=network, timeout=timeout)
+
+    @classmethod
+    def from_settings(cls, settings: Database, **kwargs):
+        kwargs = kwargs.copy()
+        sentry_name = kwargs.pop("sentry_name")
+        sentry_hash = kwargs.pop("sentry_hash")
+        endpoint_url = settings.parameters.pop("endpoint_url")
+        network = settings.parameters.pop("network")
+        timeout = settings.parameters.pop("timeout", 60)
+        kwargs.update(settings.parameters)
+        return cls(
+            endpoint_url=endpoint_url,
+            network=network,
+            timeout=timeout,
+            sentry_name=sentry_name,
+            sentry_hash=sentry_hash,
+            **kwargs,
+        )
 
     async def get(self, tx_hash: str) -> Trace:
         """
