fcab37d0c6cdeb9298624d333bd8d85f651f4a09
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-07-05 10:41:26 +0300

    feat: add tx log entries filter by abi signatures (#343)
    * feat: add transaction utils for filtering log entries
    * feat: update filter events logic

---
+++ src/sentinel/utils/transaction.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Dict, Iterator, List
+
+from sentinel.db.contract.utils import extract_data_from_event_log
+from sentinel.models.contract import ABISignature
+from sentinel.models.transaction import LogEntry
+
+
+@dataclass
+class Event:
+    address: str
+    type: str
+    fields: Dict[str, str]
+
+
+def filter_events(log_entries: List[LogEntry], signatures: List[ABISignature]) -> Iterator[Dict]:
+    for log_entry in log_entries:
+        if len(log_entry.topics) == 0:
+            continue
+
+        signature_hash = log_entry.topics[0]
+        for signature in signatures:
+            if signature_hash == signature.signature_hash:
+                if log_entry.data == "0x":
+                    continue
+                yield Event(
+                    address=log_entry.address.lower(),
+                    type=signature.abi.name,
+                    fields=extract_data_from_event_log(
+                        abi_record=signature.abi,
+                        topics=log_entry.topics,
+                        data=log_entry.data,
+                    ),
+                )
