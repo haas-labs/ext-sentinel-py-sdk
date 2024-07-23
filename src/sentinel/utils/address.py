86a44d9fcf1623f1a71851434713451cbdc13fd6
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-07-23 18:52:07 +0300

    feat: add normalize address util and tests (#372)

--- src/sentinel/utils/address.py
+++ src/sentinel/utils/address.py
@@ -1,3 +1,16 @@
+def normalize_address(address: str) -> str:
+    """
+    Normalize addrress
+    - address must have 0x prefix
+    - use lowercase
+    - remove leading zeros
+    """
+    if address.startswith("0x"):
+        return shorten_ethereum_address(address=address.lower())
+    else:
+        return address
+
+
 def shorten_ethereum_address(address: str) -> str:
     # Check if the address is valid
     if not isinstance(address, str) or not address.startswith("0x"):
