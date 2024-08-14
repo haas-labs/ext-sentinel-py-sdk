b708c6f39f815e2bfd94d169ec161b239f4c5673
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-08-14 12:55:45 +0300

    feat: add manifest tag as string type (#404)

--- src/sentinel/manifest.py
+++ src/sentinel/manifest.py
@@ -74,8 +74,8 @@ class MetadataModel(BaseModel):
     name: str
     version: str
     description: str
-    tags: List[Tag] = Field(default_factory=list)
-    network_tags: List[NetworkTag] = Field(default_factory=list)
+    tags: List[str] = Field(default_factory=list)
+    network_tags: List[str] = Field(default_factory=list)
     faq: List[FAQModel] = Field(default_factory=list)
     status: Status
 
