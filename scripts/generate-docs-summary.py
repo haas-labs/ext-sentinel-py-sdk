3c3e6150a3f470e6edc7e5074a56cffa76b8e14c
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-10-20 18:13:24 +0300

    feat: update gitbook summary script and summary.md (#457)

--- scripts/generate-docs-summary.py
+++ scripts/generate-docs-summary.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
 
-import os
 import pathlib
 
+DOCS_DIR = pathlib.Path("docs")
+DEFAULT_INDENT = "  "
+IGNORE_LIST = ["docs/.obsidian", "docs/SUMMARY.md", "docs/README.md"]
 
-def generate_summary(path: pathlib.Path):
-    for filepath in path.glob("**/*md"):
-        rel_filepath = pathlib.Path(str(filepath).replace(str(path), ""))
 
-        if str(rel_filepath.parent) == "/":
-            print(f"- [{rel_filepath.stem}]({rel_filepath})")
+def generate_summary(path: pathlib.Path, indent_level: int = 0):
+    indent = "  " * indent_level
+    items = sorted(path.glob("*"))
+    for item in items:
+        if str(item) in IGNORE_LIST:
+            continue
+
+        # if the item is directory, process it recursively
+        if item.is_dir(follow_symlinks=False):
+            print(f"{indent}- [{str(item.name)}](#)")
+            generate_summary(item, indent_level=indent_level + 1)
         else:
-            name = os.sep.join([str(rel_filepath.parent), str(rel_filepath.stem)])
-            print(f"- [{name}]({rel_filepath})")
+            print(f"{indent}- [{item.stem}]({item.relative_to(DOCS_DIR)})")
+    # for filepath in path.glob("**/*md"):
+    #     rel_filepath = pathlib.Path(str(filepath).replace(str(path), ""))
 
-        # name = name
-        # print(name, rel_filepath)
-
-        # level = rel_filepath.count(os.sep)
-        # indent = " " * 2 * level
-        # summary_lines.append(f"{indent}* [{filepath.name}]({rel_filepath})")
-        # print(structure, rel_filepath)
-    # for root, dirs, files in os.walk(start_path):
-    # Adjust the depth level based on the root directory
-    #     for f in sorted(files):
-    #         if f.endswith(".md"):
-    #             rel_path = os.path.relpath(os.path.join(root, f), start_path)
-    #             summary_lines.append(f"{indent}* [{f}](./{rel_path})\n")
-
-    # with open(os.path.join(start_path, 'SUMMARY.md'), 'w') as summary_file:
-    #     summary_file.writelines(summary_lines)
+    #     if str(rel_filepath.parent) == "/":
+    #         print(f"- [{rel_filepath.stem}]({rel_filepath})")
+    #     else:
+    #         name = os.sep.join([str(rel_filepath.parent), str(rel_filepath.stem)])
+    #         print(f"- [{name}]({rel_filepath})")
 
 
 if __name__ == "__main__":
@@ -38,5 +36,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="generate-docs-summary")
     parser.add_argument("--path", type=pathlib.Path, required=True, help="Path to documentation")
     args = parser.parse_args()
+
+    readme_path: pathlib.Path = args.path / "README.md"
+    if readme_path.exists():
+        print("- [Introduction](README.md)")
 
     generate_summary(args.path)
