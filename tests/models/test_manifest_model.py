afc9905fcc7132f955ecc810b4fb11977aeb93a5
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-10-26 17:52:54 +0300

    test: add tests for manifest api to update ui schema (#462)

--- tests/models/test_manifest_model.py
+++ tests/models/test_manifest_model.py
@@ -1,4 +1,5 @@
 from sentinel.manifest import BaseSchema, Severity
+from sentinel.services.manifest_api import update_ui_schema
 
 
 def test_severity_model():
@@ -6,3 +7,24 @@ def test_severity_model():
     assert schema.severity == -1, "Incorrect severity value"
 
     assert Severity(schema.severity) == Severity.AUTO, "Incorrect default (AUTO) severity value"
+
+
+def test_ui_schema_handling():
+    class AddressManifest(BaseSchema):
+        address: str
+
+    class NoFieldsManifest(BaseSchema): ...
+
+    assert update_ui_schema(ui_schema={}, schema=AddressManifest) == {
+        "ui:order": ["address", "severity"]
+    }, "Incorrect UI schema"
+
+    assert update_ui_schema(ui_schema={"ui:order": ["severity", "address"]}, schema=AddressManifest) == {
+        "ui:order": ["address", "severity"]
+    }, "Incorrect UI schema"
+
+    assert update_ui_schema(ui_schema={}, schema=NoFieldsManifest) == {"ui:order": ["severity"]}, "Incorrect UI schema"
+
+    assert update_ui_schema(ui_schema={"ui:order": ["severity"]}, schema=NoFieldsManifest) == {
+        "ui:order": ["severity"]
+    }, "Incorrect UI schema"
