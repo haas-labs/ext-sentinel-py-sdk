3552a5b3ca86a06dd4e95022d92a71c703ec73f3
Andrii Usov <146418764+ausov-hacken@users.noreply.github.com>
2024-11-19 15:17:59 +0200

    test: add tests for leading zero address handling (#476)
    * feat: add ignore macos specific files
    * feat: add descriptions for utils/address + add tests

--- src/sentinel/utils/address.py
+++ src/sentinel/utils/address.py
@@ -1,9 +1,28 @@
+"""
+This module provides utility functions for normalizing and shortening Ethereum addresses.
+
+Functions:
+    normalize_address(address: str) -> str:
+        Normalize an Ethereum address by ensuring it has a '0x' prefix, is in lowercase, and has no leading zeros.
+    shorten_ethereum_address(address: str) -> str:
+        Shorten an Ethereum address by removing leading zeros and ensuring it is 40 characters long.
+"""
+
+
 def normalize_address(address: str) -> str:
     """
-    Normalize addrress
-    - address must have 0x prefix
-    - use lowercase
-    - remove leading zeros
+    Normalize an Ethereum address.
+
+    This function ensures that the address:
+    - Has a '0x' prefix.
+    - Is in lowercase.
+    - Has no leading zeros.
+
+    Args:
+        address (str): The Ethereum address to normalize.
+
+    Returns:
+        str: The normalized Ethereum address.
     """
     if address.startswith("0x"):
         return shorten_ethereum_address(address=address.lower())
@@ -12,14 +31,26 @@ def normalize_address(address: str) -> str:
 
 
 def shorten_ethereum_address(address: str) -> str:
+    """
+    Shortens an Ethereum address by removing leading zeros and ensuring it is 40 characters long.
+
+    Args:
+        address (str): The Ethereum address to be shortened. It must be a string starting with "0x".
+
+    Returns:
+        str: The shortened Ethereum address in the correct format.
+
+    Raises:
+        ValueError: If the provided address is not a valid Ethereum address format.
+    """
     # Check if the address is valid
     if not isinstance(address, str) or not address.startswith("0x"):
         raise ValueError("Invalid Ethereum address format")
 
     address_body = address[2:]
-    
+
     if len(address_body) > 40:
-        address_body = address_body.lstrip('0')
+        address_body = address_body.lstrip("0")
 
     address_body = address_body.zfill(40)
 
